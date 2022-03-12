47388cecb6bf1fa2fd6768cb8f98684f94ba65ab
CHOHYUNSIK <whgustlr0326@gmail.com>
2022-03-12 17:00:14 +0900

    update

--- DataScience/MatPlotLibExercise.py
+++ DataScience/MatPlotLibExercise.py
@@ -30,10 +30,8 @@ for student_bmi in bmi:
         students[3] += 1
 
 plt.bar(bmi_level, students)
-plt.savefig('Barchart.png')
+plt.show()
 
-# clear plt
-plt.clf()
 
 # Histogram
 plt.hist(bmi, bins = [bmi.min(), 18.5, 25.0, 30.0, bmi.max()])
@@ -41,9 +39,29 @@ plt.title("histogram of result")
 plt.xticks([bmi.min(), 18.5, 25.0, 30.0, bmi.max()])
 plt.xlabel('BMI')
 plt.ylabel('number of students')
-plt.savefig('Histogram.png')
+plt.show()
+
 
 # Pie chart
+bmi_level=['Underweight','Healthy','Overweight','Obese']
+students=[0,0,0,0]
+
+for student_bmi in bmi:
+    if student_bmi < 18.5:
+        students[0] += 1
+    elif 18.5 <= student_bmi <= 24.9:
+        students[1] += 1
+    elif 25.0 <= student_bmi <= 29.9:
+        students[2] += 1
+    elif 30.0 <= student_bmi:
+        students[3] += 1
+
+plt.pie(students, labels=bmi_level, autopct='%1.2f%%')
+plt.show()
 
 
 # Scatter plot
+plt.scatter(wt, ht, color='b')
+plt.xlabel('weight(kg)')
+plt.ylabel('height(cm)')
+plt.show()
