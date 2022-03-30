071c02c9e1bb6d3fb3c165467a5fbe02673ef45f
CHOHYUNSIK <whgustlr0326@gmail.com>
2022-03-30 23:38:43 +0900

    remove data science dir

--- Python/Main.py
+++ Python/Main.py
@@ -1,8 +1,3 @@
-import pandas as pd
-import numpy as np
+import featuretools as ft
 
-
-numpyArray = np.array([3. , '?', 2. , 5. , '*' , 4. , 5. , 6. , '+' , 3., 2. , '&' , 5. , '?' , 7. , '!']).reshape(4,4)
-
-df = pd.DataFrame(numpyArray, columns=['column_1','column_2','column_3','column_4'])
-print(df)
+print(ft.__version__)
