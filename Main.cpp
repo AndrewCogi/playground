6e95b0bf5501d22c3c64dd6fd59d87b2672f1b8a
CHOHYUNSIK <whgustlr0326@gmail.com>
2022-06-26 02:04:06 +0900

    update

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main(void){
-	int n;
-	cin >> n;
-	int arr1[n];
-	int arr2[n];
-	for(int i=0; i<n; i++){
-		cin >> arr1[i];
-	}
-	for(int i=0; i<n; i++){
-		cin >> arr2[i];
-	}
-	// sorting
-	sort(arr1,arr1+n);
-	sort(arr2,arr2+n);
-	// cal
-	int result = 0;
-	for(int i=0; i<n; i++){
-		result += arr1[i]*arr2[n-i-1];
-	}
-	cout << result;
+	int n,m;
+	cin >> n >> m;
+	cout << (n+m);
 	return 0;
 }
